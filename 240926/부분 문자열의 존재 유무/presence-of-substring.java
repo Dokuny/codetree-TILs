9006e965a87af113e7220f8cffb8234abe339622
Dohun Lee  🍀 <nestour95@gmail.com>
2024-09-26 10:04:52 +0900

    Title: 부분 문자열의 존재 유무, Time: 76ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240926/부분 문자열의 존재 유무/presence-of-substring.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        boolean isEE = false;
+        boolean isEA = false;
+
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i-1) == 'e') {
+                if(s.charAt(i) == 'e') {
+                    isEE = true;
+                }
+
+                if(s.charAt(i) == 'a') {
+                    isEA = true;
+                }
+            }
+        }
+
+        String answer = "";
+
+        if(isEE) answer += "YES";
+    
+
+        if(isEA) answer += " YES";
+
+        System.out.println(answer.trim());
+        
+    }
+}
