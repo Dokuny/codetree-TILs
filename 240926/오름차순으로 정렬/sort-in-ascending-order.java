23d2b9c05eb52e0a81add30f39dc5381075e6f8d
Dohun Lee  🍀 <nestour95@gmail.com>
2024-09-26 16:43:58 +0900

    Title: 오름차순으로 정렬, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240926/오름차순으로 정렬/sort-in-ascending-order.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i : arr) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
