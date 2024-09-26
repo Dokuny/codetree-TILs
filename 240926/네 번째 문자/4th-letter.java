4574e34323358b5781d4a66f7e8a27ead72f341b
Dohun Lee  🍀 <nestour95@gmail.com>
2024-09-26 10:15:36 +0900

    Title: 네 번째 문자, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240926/네 번째 문자/4th-letter.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        char c = st.nextToken().charAt(0);
+
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        for(int i = 0; i < N; i++) {
+            String word = br.readLine();
+
+            boolean isC = false;
+            for(int j = 0; j < word.length(); j++) {
+                if(word.charAt(j) == c) {
+                    cnt++;
+                    isC = true;
+                }
+            }
+
+            if(isC) sb.append(word).append("\n");
+        }
+
+        System.out.println(cnt+"\n"+sb.toString());
+    }
+}
