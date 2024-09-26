f68820239678d85d5c5d998f697f09f94535f6e8
Dohun Lee  🍀 <nestour95@gmail.com>
2024-09-26 11:55:35 +0900

    Title: 진법 변환 4, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240926/진법 변환 4/base-conversion-4.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine(), 2));
+    }
+}
