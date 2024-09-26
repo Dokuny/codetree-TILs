8eab566c4ed85adde259db091d9ffc4832696df9
Dohun Lee  🍀 <nestour95@gmail.com>
2024-09-26 11:54:22 +0900

    Title: 진법 변환 3, Time: 250ms, Memory: 21MB, Status: Passed - CodeTree

--- 240926/진법 변환 3/base-conversion-3.java
+++ 240926/진법 변환 3/base-conversion-3.java
@@ -6,7 +6,33 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(Long.toString(Long.parseLong(br.readLine(), 8),2));
+        // 세자리씩 끊어서 변환
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+
+        for(int i = 0; i < input.length(); i++) {
+            int num = input.charAt(i) - '0';
+
+            StringBuilder bSb = new StringBuilder();
+
+            while(num != 0) {
+                bSb.append(num % 2);
+                num /= 2;
+            }
+            if(i !=0 && bSb.length() < 1) {
+                bSb.append("0");
+            }
+
+            if(i !=0 && bSb.length() < 2) {
+                bSb.append("0");
+            }
+
+            if(i !=0 && bSb.length() < 3) bSb.append("0");
+            
+            sb.append(bSb.reverse());
+        }
+        System.out.println(sb);
 
     }
 }
