21bb653aaaa74dd43b51c97cd0cb2ea633dd385c
Dohun Lee  🍀 <nestour95@gmail.com>
2024-10-08 16:06:48 +0900

    Title: 0에 가장 가까운 합 3, Time: 729ms, Memory: 27MB, Status: Passed - CodeTree

--- 241008/0에 가장 가까운 합 3/sum-closest-to-zero-3.java
+++ 241008/0에 가장 가까운 합 3/sum-closest-to-zero-3.java
@@ -8,56 +8,26 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        ArrayList<Integer> list = new ArrayList<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        HashSet<Integer> set = new HashSet<>();
-
         for(int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = Math.abs(num);
-            set.add(num);
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        Arrays.sort(arr);
+        list.sort(Comparator.comparingInt(Math::abs));
 
         // 슬라이딩 윈도우로 diff 계산
-        int max = Integer.MAX_VALUE;
         int answer = Integer.MAX_VALUE;
+        int check = Integer.MAX_VALUE;
         for(int i = 0; i < n-1; i++) {
-            int diff = arr[i+1] - arr[i];
-
-            if(diff < max) {
-                // 가능한 수인지 검증
-                // a가 음수이고 b가 양수
-                if(set.contains(-arr[i+1]) && set.contains(arr[i])){
-                    max = diff;
-                    answer = -arr[i+1] + arr[i];
-                }else if(set.contains(arr[i+1]) && set.contains(-arr[i])){
-                    max = diff;
-                    answer = arr[i+1] + -arr[i];
-                }else if(set.contains(-arr[i]) && set.contains(-arr[i+1])){
-                    // a가 음수 b가 음수
-                    int sum = -arr[i] + -arr[i+1];
-                    if(Math.abs(sum) <= max) {
-                        max = Math.abs(sum);
-                        answer = sum;
-                    }
-                }else {
-                    // a가 양수 b가 양수
-                    int sum = arr[i] + arr[i+1];
-                    if(sum <= max) {
-                        max = sum;
-                        answer = sum;
-                    }
-                }
-            }
+            int sum = list.get(i) + list.get(i+1);
+            if(check > Math.abs(sum)) {
+                answer = sum;
+                check = Math.abs(sum);
+            } 
         }
-
-
-
-
         System.out.println(answer);
         
     }
